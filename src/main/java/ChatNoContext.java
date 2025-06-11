03d7618a496460681a3e0691c341c59541a54c94
Frank Greco <fgreco@javasig.com>
2025-06-11 15:19:33 -0400

    Demonstrations of simple chat apps without and with context.

--- src/main/java/ChatNoContext.java
+++ src/main/java/ChatNoContext.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
+
+import java.util.Scanner;
 
 public class ChatNoContext {
+    public static void main(String[] args) {
+        Scanner userinput;      // user inputted line as a Scanner
+        String cmdline;
+
+        ChatModel cmodel = OpenAiChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName(OpenAiChatModelName.GPT_4_O)
+                .build();
+
+        while (true) {
+            System.out.print("prompt> ");
+
+            userinput = new Scanner(System.in);
+            cmdline = userinput.nextLine();
+
+            if (cmdline.isBlank())       // If nothing, do nothing
+                continue;
+
+            UserMessage usrmsg = UserMessage.from(cmdline);
+            var answer = cmodel.chat(usrmsg);
+
+            System.out.println(answer.aiMessage().text());
+        }
+    }
 }
--- src/main/java/ChatWithContext.java
+++ src/main/java/ChatWithContext.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
+
+import java.util.Scanner;
 
 public class ChatWithContext {
+    public static void main(String[] args) {
+        Scanner userinput;      // user inputted line as a Scanner
+        String cmdline;
+
+        ChatModel cmodel = OpenAiChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName(OpenAiChatModelName.GPT_4_O)
+                .build();
+
+        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(100);
+
+        while (true) {
+            System.out.print("prompt> ");
+
+            userinput = new Scanner(System.in);
+            cmdline = userinput.nextLine();
+
+            if (cmdline.isBlank())       // If nothing, do nothing
+                continue;
+
+            UserMessage usrmsg = UserMessage.from(cmdline);   // create the prompt
+            chatMemory.add(usrmsg);
+
+            var answer = cmodel.chat(chatMemory.messages());
+            var response = answer.aiMessage().text();
+
+            System.out.println(response);
+
+            chatMemory.add(UserMessage.from(response)); // Add response from llm
+            chatMemory.add(UserMessage.from(cmdline));  // Add prompt from the user
+        }
+    }
 }
