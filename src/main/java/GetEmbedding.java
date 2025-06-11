6b612d80da5818bebae1b2c123be160a9c57033f
Frank Greco <fgreco@javasig.com>
2025-06-11 15:20:23 -0400

    Shows how to get embeddings.

--- src/main/java/GetEmbedding.java
+++ src/main/java/GetEmbedding.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
+import dev.langchain4j.model.output.Response;
+
+import static dev.langchain4j.model.openai.OpenAiEmbeddingModelName.TEXT_EMBEDDING_3_SMALL;
 
 public class GetEmbedding {
+
+    public static void main(String[] argv) {
+        String apiKey = System.getenv("OPENAI_API_KEY");
+
+        EmbeddingModel model = OpenAiEmbeddingModel.builder()
+                .apiKey(apiKey)
+                .modelName(TEXT_EMBEDDING_3_SMALL)
+                .build();
+
+        Response<Embedding> response = model.embed("One fish, two fish, red fish, blue fish");
+        Embedding embedding = response.content();
+
+        System.out.println(embedding);
+    }
 }
+
