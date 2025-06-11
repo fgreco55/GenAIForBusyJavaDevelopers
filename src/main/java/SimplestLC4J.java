755e3505486c2adbf08fdf41a5483ca7f5c3bd83
Frank Greco <fgreco@javasig.com>
2025-06-11 15:24:09 -0400

    Demonstrates how simple the LC4J API is.

--- src/main/java/SimplestLC4J.java
+++ src/main/java/SimplestLC4J.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
 
 public class SimplestLC4J {
+    public static void main(String[] args) {
+        ChatModel cmodel = OpenAiChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName(OpenAiChatModelName.GPT_4_O)
+                .build();
+
+        System.out.println(cmodel.chat("What are the benefits of Java"));
+    }
 }
