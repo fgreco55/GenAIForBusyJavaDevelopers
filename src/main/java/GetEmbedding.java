defb3b6b4174f9c5292423f64004f75712b250a5
Frank Greco <fgreco@javasig.com>
2026-04-22 12:02:19 -0400

    Output the dimensions in addition to the vector

--- src/main/java/GetEmbedding.java
+++ src/main/java/GetEmbedding.java
@@ -1,12 +1,11 @@
-import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.model.openai.OpenAiEmbeddingModel;
 import dev.langchain4j.model.output.Response;
 
 import static dev.langchain4j.model.openai.OpenAiEmbeddingModelName.TEXT_EMBEDDING_3_SMALL;
 
 public class GetEmbedding {
-
     public static void main(String[] argv) {
         String apiKey = System.getenv("OPENAI_API_KEY");
 
@@ -15,10 +14,9 @@ public class GetEmbedding {
                 .modelName(TEXT_EMBEDDING_3_SMALL)
                 .build();
 
-        Response<Embedding> response = model.embed("Imagination is more important than knowledge");
+        Response<Embedding> response = model.embed("One fish, two fish, red fish, blue fish");
         Embedding embedding = response.content();
 
-        System.out.println(embedding);
+        System.out.println(embedding.dimension() + " elements: " + embedding);
     }
 }
-
